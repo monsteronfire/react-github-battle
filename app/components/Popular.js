f3889ca67f8e7fc28e67924de90d6ff61ea4a1b7
Shaii Ong <monsteronfire@gmail.com>
2017-06-09 21:25:56 +0800

    Add tab ui and accompanying function

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,9 +1,38 @@
 const React = require('react');
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState(() => {
+      return {
+        selectedLanguage: lang
+      }
+    });
+  }
+
   render() {
+    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
     return (
-      <div>Popular</div>
+      <ul className='languages'>
+        {languages.map((lang) => {
+            return (
+              <li
+                onClick={this.updateLanguage.bind(null, lang)}
+                key={lang}>
+                {lang}
+              </li>
+            )
+          }, this)}
+      </ul>
     )
   }
 }
