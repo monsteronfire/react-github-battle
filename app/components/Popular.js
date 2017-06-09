873793c9fb3cb727288e76e7623f2404a16bb820
Shaii Ong <monsteronfire@gmail.com>
2017-06-09 21:32:10 +0800

    Add styles on tab click

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -26,12 +26,13 @@ class Popular extends React.Component {
         {languages.map((lang) => {
             return (
               <li
+                style={lang === this.state.selectedLanguage ? { color: '#d0021b'} : null}
                 onClick={this.updateLanguage.bind(null, lang)}
                 key={lang}>
                 {lang}
               </li>
             )
-          }, this)}
+          })}
       </ul>
     )
   }
