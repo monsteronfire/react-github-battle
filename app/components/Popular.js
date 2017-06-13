55e6ed2314fac21de5915a580a3bd16ef1a0bbe7
Shaii Ong <monsteronfire@gmail.com>
2017-06-13 23:00:10 +0800

    Add UI for repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -21,6 +21,36 @@ function SelectLanguage(props) {
   )
 }
 
+function RepoGrid(props) {
+  return (
+    <ul className='popular-list'>
+      {props.repos.map((repo, index) => {
+        return(
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={`Avatar for ${repo.owner.login}` }
+                />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
+
 SelectLanguage.propTypes = {
   selectedLanguage: PropTypes.string.isRequired,
   onSelect: PropTypes.func.isRequired
@@ -65,7 +95,9 @@ class Popular extends React.Component {
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage}
         />
-        {JSON.stringify(this.state.repos, null, 2)}
+        {!this.state.repos
+            ? <p>Loading...</p>
+            : <RepoGrid repos={this.state.repos} /> }
     </div>
     )
   }
