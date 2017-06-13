b69f50ff0f6ca46893c4b6681107b26c8a674b23
Shaii Ong <monsteronfire@gmail.com>
2017-06-13 22:11:16 +0800

    Get popular repos from github api via axios

---
+++ app/utils/api.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+module.exports = {
+  fetchPopularRepos: (language) => {
+    let popularRepoURL = 'https://api.github.com/search/repositories?q=stars:>1+language';
+    let encodedURI = window.encodeURI(`${popularRepoURL}${language}&sort=stars&order=desc&type=Repositories`);
+
+    return axios.get(encodeURI)
+      .then((response) => {
+        return response.data.items;
+      });
+  }
+};
